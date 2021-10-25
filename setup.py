b02bb5ba7a03d3ade6ae51e84d71b20b8b00069f
chagama-g <55901504+chagama-g@users.noreply.github.com>
2021-10-25 13:20:10 +0900

    Create package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='slider_mp',
+    version='0.1',
+    # packages=['slider_mp'],
+    packages=find_packages(),
+    url='',
+    license='',
+    author='chagama-g',
+    author_email='55901504+chagama-g@users.noreply.github.com',
+    description=''
+)
---
+++ slider_mp/__init__.py
@@ -0,0 +1,1 @@
+from .slider_mp import SliderMP
