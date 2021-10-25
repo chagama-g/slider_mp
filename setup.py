be3ec786f0d5c9839eb72fc8d58e043bb3ee9972
chagama-g <55901504+chagama-g@users.noreply.github.com>
2021-10-25 13:32:00 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='slider_mp',
     version='0.1',
-    # packages=['slider_mp'],
     packages=find_packages(),
-    url='',
-    license='',
+    url='https://github.com/chagama-g/slider_mp',
+    license='MIT License',
     author='chagama-g',
     author_email='55901504+chagama-g@users.noreply.github.com',
-    description=''
+    description='multiprocessing によってnon-blockingな Slider を表示する',
+    install_requires=["matplotlib"]
 )
